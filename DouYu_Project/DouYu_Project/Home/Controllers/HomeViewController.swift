8768b4c1a4568ae4ffad4025e06f6c694cc374d4
崔林豪 <15021538370@163.com>
2021-11-06 18:02:01 +0800

    tianjia title view

--- DouYu_Project/DouYu_Project/Home/Controllers/HomeViewController.swift
+++ DouYu_Project/DouYu_Project/Home/Controllers/HomeViewController.swift
@@ -7,36 +7,45 @@
 
 import UIKit
 
+private let kTitleViewH : CGFloat = 40
 class HomeViewController: UIViewController {
+	
+	fileprivate lazy var pageTitleView: PageTitleView = {
+		
+		let titles = ["推荐", "游戏","娱乐", "去玩","魔兽","三元","chinaJoy","西游记"]
+		let titleFrame = CGRect(x: 0, y: kStatusBarH + kNavigationBarH - 44, width: kScreenH, height:40)
+		let titleView = PageTitleView(frame: titleFrame, titles: titles)
+		
+		//codeTest
+		//titleView.backgroundColor = .systemRed
+		
+		return titleView
+	}()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-	
         view.backgroundColor = .white
-        self.tabBarItem.title = "主页"
-		self.navigationController?.navigationBar.barTintColor =
-			UIColor(red: 55/255, green: 186/255, blue: 89/255, alpha: 1)
-		//修改导航栏文字颜色
-		//self.navigationController?.navigationBar.titleTextAttributes =
-			//[NSForegroundColorAttributeName: UIColor.white]
-		self.navigationController?.navigationBar.setBackgroundImage(UIColor.systemGray.toImage(), for: .default)
-		
-		self.navigationController?.navigationBar.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 200)
-		
-		configureNavigaiton()
+        
+		initUI()
     }
     
-	
-	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-		let vc = DetailViewController()
-		self.navigationController?.pushViewController(vc, animated: true)
-	}
     
 }
 
 
 //MARK:- initUI
+extension HomeViewController {
+	
+	func initUI() {
+		
+		configureNavigaiton()
+		
+		view.addSubview(pageTitleView)
+		
+	}
+}
+
 extension HomeViewController {
 	
 	func configureNavigaiton()  {
@@ -51,6 +60,27 @@ extension HomeViewController {
 		let qrcodeItem = UIBarButtonItem(imageName: "Image_scan", highImageName: "Image_scan_click", size: size)
 		
 		navigationItem.rightBarButtonItems = [historyItem, searchItem, qrcodeItem]
+		
+		//有作用
+		navigationController?.navigationBar.backgroundColor = UIColor.lightGray
+		//navigationController?.navigationBar.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 200)
+		//self.navigationController?.navigationBar.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 200)
+		
+	}
+	
+}
+
+
+//MARK:- Action
+extension HomeViewController {
+	
+	func test()  {
+		
+	}
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		let vc = DetailViewController()
+		self.navigationController?.pushViewController(vc, animated: true)
 	}
 	
 }
--- DouYu_Project/DouYu_Project/Main/View/PageTitleView.swift
+++ DouYu_Project/DouYu_Project/Main/View/PageTitleView.swift
@@ -6,15 +6,153 @@
 //
 
 import UIKit
+import SwiftUI
+
+/** 只能被类遵守的协议
+
+protocol testProtocol1: class {
+	
+}
+
+protocol test2: AnyObject {
+	
+}
+
+@objc protocol test3 {
+	
+}
+ */
+
+private let kScrollLineH : CGFloat =  2
+private let kNormalColor: (CGFloat, CGFloat, CGFloat) = (85, 85, 85)
+private let kSelectColor:(CGFloat, CGFloat, CGFloat) = (255, 128, 0)
+
+
+//MARK:- 定义协议
+protocol PageTitleViewDelegate: AnyObject {
+	func pagetitleView(_ titleView: PageTitleView, selectedIndex index: Int)
+}
 
 class PageTitleView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+	fileprivate var currentIndex  = 0
+	fileprivate var titles: [String]
+	
+	fileprivate lazy var titleLabes : [UILabel] = [UILabel]()
+	
+	fileprivate lazy var scrolleView: UIScrollView = {
+		
+		let scrolleView = UIScrollView()
+		scrolleView.showsHorizontalScrollIndicator = false
+		scrolleView.scrollsToTop = false
+		//设置边界
+		scrolleView.bounces = false
+		
+		return scrolleView
+	}()
+	
+	fileprivate	lazy var scrollLine: UIView = {
+		let line = UIView()
+		line.backgroundColor = .orange
+		return line
+	}()
+	
+	  init(frame: CGRect, titles: [String]) {
+		
+		self.titles = titles
+		super.init(frame: frame)
+		
+		initUI()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
 
 }
+
+extension PageTitleView {
+	
+	func initUI() {
+		
+		addSubview(scrolleView)
+		scrolleView.frame = bounds
+		
+		setUpTitleLabels()
+		
+		setUpBottomLine()
+		
+	}
+	
+	func setUpTitleLabels() {
+		
+		let labelW: CGFloat = frame.width / CGFloat(titles.count)
+		let labelH: CGFloat = frame.height - kScrollLineH
+		let labelY: CGFloat = 0
+		
+		for (index, title) in titles.enumerated() {
+			
+			let label = UILabel()
+			
+			label.text = title
+			label.tag = index
+			label.font = UIFont.systemFont(ofSize: 16)
+			label.textColor = UIColor(r: kNormalColor.0, g: kNormalColor.1, b: kNormalColor.2)
+			label.textAlignment = .center
+			
+			let labelX  = labelW * CGFloat(index)
+			label.frame = CGRect(x: labelX, y: labelY, width: labelW, height: labelH)
+			
+			scrolleView.addSubview(label)
+			titleLabes.append(label)
+			
+			//添加手势
+			label.isUserInteractionEnabled = true
+			
+			let tapGesture = UITapGestureRecognizer(target: self, action: #selector(titleLabelClick(_:)))
+			
+			label.addGestureRecognizer(tapGesture)
+		}
+	}
+	
+	func setUpBottomLine() {
+		
+		//添加底线
+		let bottomLine = UIView()
+		bottomLine.backgroundColor = UIColor.lightGray
+		
+		let lineH : CGFloat = 0.5
+		bottomLine.frame = CGRect(x: 0, y: frame.height - lineH, width: frame.width, height: lineH)
+		addSubview(bottomLine)
+		
+		//添加滑动线
+		guard let firstLabel = titleLabes.first else { return }
+		firstLabel.textColor = UIColor(r: kSelectColor.0, g: kSelectColor.1, b: kSelectColor.2)
+		
+		scrolleView.addSubview(scrollLine)
+		scrollLine.frame = CGRect(x: firstLabel.frame.origin.x, y: frame.height - kScrollLineH, width: firstLabel.frame.width, height: kScrollLineH)
+		
+	}
+}
+
+//MARK:- 监听lebl点击
+
+//extension PageTitleView {
+//	
+//	@objc fileprivate func titleLabelClick(_ tapGes: UITapGestureRecognizer) {
+//		
+//		guard let currentLabel = tapGes.view as? UILabel else { return }
+//		
+//		if currentLabel.tag == currentIndex { return }
+//			
+//		let oldLabel = titleLabes[currentIndex]
+//			
+//			currentLabel.textColor = UIColor(r: kSelectColor.0, g: kSelectColor.1, b: kSelectColor.2)
+//			
+//			currentIndex = currentLabel.tag
+//			
+//		
+//	}
+//	
+//	
+//}
